ce77e03ef6c9f31373866176d3bdf0e81850772f
Waldemar Krasnodębski <waldemar.krasnodebski@symfonia.pl>
2023-10-17 08:58:38 +0200

    ts-classes

--- LMJavaScript/functions.js
+++ LMJavaScript/functions.js
@@ -11,3 +11,11 @@ let name = {
 
 fullName.call(name, "Mlamla", "Blabla");
 fullName.apply(name, ["Mlamla", "Blabla"]);
+
+const add = (...numbers) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addNumbers = add(1, 2, 5, 7, 9, 4);
